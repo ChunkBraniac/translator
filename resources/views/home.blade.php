42ad4df673bd9ec61de5ecde4b328ad757489a91
ChunkBraniac <anthonyobah37@gmail.com>
2024-06-23 08:22:15 +0100

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,8 @@
     <title>Translator Project</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ url('styles.css') }}">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
@@ -15,7 +17,7 @@
             <h1>Language Translator</h1>
         </header>
 
-        <form action="{{route('translate')}}" method="post">
+        <form action="{{ route('translate') }}" method="post">
 
             @csrf
 
@@ -23,18 +25,28 @@
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="inputText">Enter Text</label>
-                        <textarea class="form-control" id="inputText" rows="6" placeholder="Type your text here..." name="inputText" @required(true)></textarea>
+                        <textarea class="form-control" id="inputText" rows="6" placeholder="Type your text here..." name="inputText"
+                            @required(true)></textarea>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="outputText">Translated Text</label>
-                        <textarea class="form-control" id="outputText" rows="6" placeholder="Translation will appear here..." name="outputText" @readonly(true)>{{ isset($translated_text) ? $translated_text : '' }}</textarea>
+                        <textarea class="form-control" id="outputText" rows="6" placeholder="Translation will appear here..."
+                            name="outputText" @readonly(true)>{{ isset($translated_text) ? $translated_text : '' }}</textarea>
+
+                        @if (!isset($translated_text))
+                            <button type="button" class="btn mt-2 copy-btn" @disabled(true)
+                                onclick="copyToClipboard()"><i class="fa-solid fa-clipboard" style="font-size: 20px;"></i></button>
+                        @else
+                            <button type="button" class="btn mt-2 copy-btn"
+                                onclick="copyToClipboard()"><i class="fa-solid fa-clipboard" style="font-size: 20px;"></i></button>
+                        @endif
                     </div>
                 </div>
             </div>
 
-            
+
             <div class="row my-4">
                 <div class="col-md-6">
                     <div class="form-group">
@@ -68,11 +80,20 @@
         </form>
 
         <div class="loader" id="loader"></div>
-        
+
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="script.js"></script>
+
+    <script>
+        function copyToClipboard() {
+            var copyText = document.getElementById("outputText");
+            copyText.select();
+            document.execCommand("copy");
+            alert("Copied to clipboard: " + copyText.value);
+        }
+    </script>
 </body>
 
 </html>
